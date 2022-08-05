9b49ad7ce6e7e1a07302e2343696d6324c57402c
Vulikh <fed.vul@icloud.com>
2022-08-05 04:14:50 +0300

    first commit for lesson 2.2

--- Lesson 2.2/ViewController.swift
+++ Lesson 2.2/ViewController.swift
@@ -9,11 +9,56 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var mainView: UIView!
+    @IBOutlet weak var redColorSlider: UISlider!
+    @IBOutlet weak var greenColorSlider: UISlider!
+    @IBOutlet weak var blueColorSlider: UISlider!
+    
+  
+    
+    @IBOutlet weak var redColorValue: UILabel!
+    @IBOutlet weak var greenColorValue: UILabel!
+    @IBOutlet weak var blueColorValue: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        mainView.layer.cornerRadius = 10
+        redColorValue.text = String(format: "%.3f", redColorSlider.value)
+        greenColorValue.text = String(format: "%.3f", greenColorSlider.value)
+        blueColorValue.text = String(format: "%.3f", blueColorSlider.value)
+        changeViewColor()
+    }
+    
+    func changeViewColor() {
+        mainView.backgroundColor = UIColor(
+            red: CGFloat(redColorSlider.value),
+            green: CGFloat(greenColorSlider.value),
+            blue: CGFloat(blueColorSlider.value),
+            alpha: 1
+        )
+    }
+    
+    
+    @IBAction func redColorSliderValueChange() {
+        changeViewColor()
+        redColorValue.text = String(format: "%.3f", redColorSlider.value)
     }
 
+
+    @IBAction func greenColorSliderValueChange() {
+        changeViewColor()
+        greenColorValue.text = String(format: "%.3f", greenColorSlider.value)
+    }
+
+    
+    @IBAction func blueColorSliderValueChange() {
+        changeViewColor()
+        blueColorValue.text = String(format: "%.3f", blueColorSlider.value)
+    }
+    
 
 }
 
